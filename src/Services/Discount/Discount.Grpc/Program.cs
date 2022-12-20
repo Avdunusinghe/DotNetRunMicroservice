970193df0541921f6d631431797d4f5ee0dddc42
Avdunusinghe <avdunusinghe@gmail.com>
2022-12-20 20:16:41 +0530

    feat : add grpc

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,4 +1,6 @@
-using Discount.Grpc.Services;
+using Discount.Grpc.Extensions;
+using Discount.Grpc.Repositories;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +8,14 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 builder.Services.AddGrpc();
 
 var app = builder.Build();
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+//app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
